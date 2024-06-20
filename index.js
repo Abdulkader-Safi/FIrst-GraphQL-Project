ea62fb22595f9ae82169cfa27f6e9020ab0c7b71
Abdulkader Safi <53955839+Abdulkader-Safi@users.noreply.github.com>
2024-06-21 02:34:18 +0300

    add search for Games and Game with id, Reivews and Review with id, Author and Author with id

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ const resolvers = {
     games() {
       return db.games;
     },
+    game(_, args) {
+      return db.games.find((g) => g.id === args.id);
+    },
     authors() {
       return db.authors;
     },
+    author(_, args) {
+      return db.authors.find((a) => a.id === args.id);
+    },
     reviews() {
       return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((r) => r.id === args.id);
     },
   },
 };
